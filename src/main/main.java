2d02dccc8c4dc20d49afcafa44fb1707b2e25447
JodanBJr23 <jordanmarz12@gmail.com>
2023-03-30 09:08:41 -0600

    prueba
    prueba

--- src/main/main.java
+++ src/main/main.java
@@ -4,6 +4,6 @@ package main;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hola grupo");
+        System.out.println("Hola grupo");System.out.println(" prueba");
     }
 }
